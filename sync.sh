44cb1aed6f566aa927e72ee22ca6810a1041db56
carlo <carlodef@gmail.com>
2012-05-07 14:39:32 +0200

    delete obsolete scripts

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssh fuchsia "cd ipol/; ./stop.sh";
-
-rsync -avz --delete -e ssh ./ fuchsia:~/ipol/demo/ --exclude=demo.conf* --exclude=app/*/tmp/ --exclude=app/*/archive/ --exclude=app/*/bin/ --exclude=app/*/src/ --exclude=app/*/dl/ --exclude=.git;
-
-ssh fuchsia "cd ipol/demo/; rm -r app/*/tmp app/*/bin/ app/*/dl";
-
-ssh fuchsia "cd ipol/; ./start.sh"
