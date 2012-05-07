0082c6933a075afffe7c97e5becb0a600dc1c90b
carlo <carlodef@gmail.com>
2012-05-07 15:55:38 +0200

    modification (commented) to choose which apps to build and run

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,15 @@ import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
+# List of demos considered
+demos_list = ['answer_old_question']
+
 # (id:app) demo dict
 demo_dict = dict([(demo_id,
                    # function version of `from demo_id import app as _.app`
                    __import__(demo_id, globals(), locals(), ['app'], -1).app)
                   for demo_id in os.listdir(base_dir)
+                  # for demo_id in demos_list 
                   if os.path.isdir(os.path.join(base_dir, demo_id))])
                   
 
