268b258e3ec7eb940160285619689877c3aee0c8
carlo <carlodef@gmail.com>
2012-09-28 17:51:14 +0200

    fix bug

--- app/d_angulo/scripts/run_blur.sh
+++ app/d_angulo/scripts/run_blur.sh
@@ -3,5 +3,5 @@
 # $1: tilt parameter
 # $2: gaussian std deviation
 
-echo "blur left_image.png left_image_blurred_for_t$1.tif $2"
-blur left_image.png left_image_blurred_for_t$1.tif $2
+echo "blur left_image.tif left_image_blurred_for_t$1.tif $2"
+blur left_image.tif left_image_blurred_for_t$1.tif $2
