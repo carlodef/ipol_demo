5c19894d86d02ee0eb49aecf5c7de6d68c80ef03
Carlo De Franchis <carlodef@gmail.com>
2012-10-02 11:19:52 +0200

    [d_angulo] modify statistics computation, to count as false matches all the rejected pixels

--- app/d_angulo/scripts/compute_statistics.sh
+++ app/d_angulo/scripts/compute_statistics.sh
@@ -7,6 +7,11 @@
 if [ "${ground_truth}" != "" ]; then 
     # exclude ground truth mask from the comparison 
     plambda ground_truth_mask.tif ground_truth.tif "x[0] 0 = inf y[0] if" > ground_truth_for_statistics.tif 
-    disp_statistics out_disp.tif ground_truth_for_statistics.tif 1 > statistics.txt 
-    disp_statistics disp_std_bm.tif ground_truth_for_statistics.tif 1 >> statistics.txt 
+    # exclude filtered pixels from the computed disparity maps 
+    plambda out_filt.tif out_disp.tif "x[0] 0 = nan y[0] if" > out_disp_for_statistics.tif 
+    plambda filt_std_bm.tif disp_std_bm.tif "x[0] 0 = nan y[0] if" > disp_std_bm_for_statistics.tif 
+
+    # compute the statistics
+    disp_statistics out_disp_for_statistics.tif ground_truth_for_statistics.tif 1 > statistics.txt 
+    disp_statistics disp_std_bm_for_statistics.tif ground_truth_for_statistics.tif 1 >> statistics.txt 
 fi 
