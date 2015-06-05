6edddb90f786f02f65787eaf50848ea87e4db158
Carlo De Franchis <carlodef@gmail.com>
2015-06-05 15:06:22 +0200

    [d_a_contrario_orientation_modes] added xlink to paper

--- app/d_a_contrario_orientation_modes/app.py
+++ app/d_a_contrario_orientation_modes/app.py
@@ -28,7 +28,7 @@ class app(base_app):
     input_ext = '.png'   # input image expected extension (ie file format)
     is_test = True       # switch to False for deployment
     is_listed = True
-    xlink_article = 'www.ipol.im'
+    xlink_article = 'http://boucantrin.ovh.hw.ipol.im/~carlo/2015_ipol_a_contrario_orientation_modes.pdf'
 
     def __init__(self):
         """
